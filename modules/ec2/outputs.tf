69f648df13c7c879cdf1b5777c2169f83a9fd53f
Sourabh Dey <sourabh.dey@technoscore.net>
2025-07-07 08:36:40 +0000

    update the image name tag

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -8,4 +8,9 @@ output "instance_public_ip" {
 
 output "instance_private_ip" {
   value = aws_instance.main.private_ip
+}
+
+output "instance_name" {
+  value = aws_instance.main.tags["Name"]
+  description = "The name tag of the EC2 instance"
 } 
--- outputs.tf
+++ outputs.tf
@@ -16,4 +16,20 @@ output "instance_public_ip" {
 
 output "instance_private_ip" {
   value = module.ec2.instance_private_ip
+}
+
+output "instance_name" {
+  value       = module.ec2.instance_name
+  description = "The name of the EC2 instance"
+}
+
+output "full_instance_details" {
+  value = {
+    name       = module.ec2.instance_name
+    id         = module.ec2.instance_id
+    public_ip  = module.ec2.instance_public_ip
+    private_ip = module.ec2.instance_private_ip
+  }
+  description = "All details about the EC2 instance"
 } 
+
