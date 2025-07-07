086cb9c5f29733f873c1f646b35413a29189b104
Sourabh Dey <sourabh.dey@technoscore.net>
2025-07-07 09:16:49 +0000

    update the backend s3 name:

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-aws01"
+    bucket         = "tf-aws-gitaction"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
